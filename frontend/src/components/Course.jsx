78f57f4a26e39854f4a1f4a4562b30c405a2e5a5
swathi_annamaneni <annamaneni@wisc.edu>
2022-10-24 23:37:47 -0500

    made a small change on course page

--- frontend/src/components/Course.jsx
+++ frontend/src/components/Course.jsx
@@ -5,7 +5,13 @@ import Reddit from "./Reddit";
 const Course = () => {
   return (
     <>
-      <div className="white-box">Madger Courses</div>
+      <div className="white-box">
+        <p style={{ fontWeight: 400 }}>Madger Courses</p>
+        <form className="search-box" style={{ height: "75%", width: "20%" }}>
+          <input type="search" />
+          <button type="submit">Search</button>
+        </form>
+      </div>
 
       <div className="grey-box">
         <h1 className="course-name">CS 577</h1>

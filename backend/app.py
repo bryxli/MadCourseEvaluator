91b2c55040feea5ed80af162e3e65c7e867ee861
Peter Bryant <72423203+peter-w-bryant@users.noreply.github.com>
2022-11-01 15:06:29 -0500

    Pop functions updated, moved MadGrades function to its own script and imported it where it is being used.

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from flask_mysqldb import MySQL
 from datetime import datetime, tzinfo
 import json
 import config # Move to gitignore
+import madgrades as mg
 
 
 app = Flask(__name__)
@@ -18,14 +19,6 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 # init MYSQL
 mysql = MySQL(app)
-
-# ~~ CONSTANTS ~~
-# 1. MadGrades
-madGrades_course_url = 'https://api.madgrades.com/v1/courses/'
-madGrades_query_url = madGrades_course_url +'?query='
-madgrades_api_token = config.madgrades_api_token                # Instantiate Madgrades API Token
-auth_header = {'Authorization': 'Token ' + madgrades_api_token} # Authorization header, required for API call
-
 
 @app.route('/', methods=['GET','POST'])
 def index():
@@ -46,21 +39,6 @@ def professorPage(pUID):
     search = pUID # TODO
     pass
 
-# TODO: Error handling
-def MadGrades(courseName):
-    """
-    Pulls grade distributions from every course at UW-Madison and populates the grade-distributions table
-    """
-    search = courseName
-    response = requests.get(madGrades_query_url + search, headers=auth_header) # API call to access list of courses matching search query
-    course_listings = response.json()                                                  
-    course_url = course_listings['results'][0]['url']                          # Get API Endpoint URL of first course in list
-    response = requests.get(course_url, headers=auth_header)                   # API call to get course data associated with select course
-    full_course_data = response.json()                                  
-    grades_url = full_course_data['gradesUrl']                                 # Get API Endpoint URL of grade distribution data                                            
-    response = requests.get(grades_url, headers=auth_header)                   # API call to access grade distribution data
-    courses = response.json()                                                  # Course offerings dictionary, with a list of dictionaries for each 
-    return(courses)
 
 if __name__ == '__main__':
     app.run(debug=True, port = 5000)
--- backend/populateDB.py
+++ backend/populateDB.py
@@ -5,6 +5,7 @@ from praw.models import MoreComments
 from RMP.ratemyprof_api import RateMyProfApi
 import json
 import mysql.connector
+import madgrades as mg
 
 # Establish connection to the database
 conn = mysql.connector.connect(
@@ -24,10 +25,6 @@ reddit = praw.Reddit(client_id = config.PRAW_client_id,
 # Instantiate PRAW Subreddit Object
 uwmadison_subreddit = reddit.subreddit('UWMadison')
 
-# Instantiate Madgrades API Token
-madgrades_api_token = config.madgrades_api_token
-auth_header = {'Authorization': 'Token ' + madgrades_api_token} # Authorization header, required for API call
-
 # Instantiate UW-Madison RateMyProfesso Object
 uwm_rmp_sid_1 = "1256"  # RMP School ID #1
 uwm_rmp_sid_2 = "18418" # RMP School ID #2
@@ -35,17 +32,11 @@ uwm_rmp_sid_2 = "18418" # RMP School ID #2
 api_1 = RateMyProfApi(uwm_rmp_sid_1, testing = True)
 api_2 = RateMyProfApi(uwm_rmp_sid_2, testing = True)
 
-
-# URL Constants
-madGrades_course_url = 'https://api.madgrades.com/v1/courses/'
-madGrades_query_url = madGrades_course_url +'?query='
-reddit_url = 'https://www.reddit.com'
-
-
 """
 Script to automate populating the MYSQL database
 """
 
+# TODO: PopCourses() is finished!
 def PopCourses():
     """
     Function to populate the courses table with all courses at UW-Madison.
@@ -84,15 +75,13 @@ def PopProfessorsHelper(endpoint):
     Helper function to populate the professors table with all professors at UW-Madison.
 
     """
-
     cursor = conn.cursor() # Create a cursor object to execute SQL queries
 
     # Iterating through every UW professor on RMP
     for key in endpoint:
         prof_json = {}                                           # Create a new JSON object for each professor
         professor = endpoint[key]                                # Get the professor object
-        prof_json['Fname'] = professor.first_name                # Get the professor's first name
-        prof_json['Lname'] = professor.last_name                 # Get the professor's last name
+        prof_json['name'] = professor.first_name + " " + professor.last_name # Get the professor's name
         prof_json['dept'] = professor.department                 # Get the professor's department
         prof_json['RMPID'] = professor.ratemyprof_id             # Get the professor's RMP ID
         prof_json['RMPRating'] = professor.overall_rating        # Get the professor's RMP rating
@@ -102,7 +91,7 @@ def PopProfessorsHelper(endpoint):
 
         try:
             # Insert course into the database's courses table
-            cursor.execute("INSERT INTO professors (pFName, pLName, pData) VALUES (%s, %s, %s)", (prof_json['Fname'], prof_json['Lname'], pData))
+            cursor.execute("INSERT INTO professors (pName, pData) VALUES (%s, %s)", (prof_json['name'], pData))
             conn.commit()
         except Exception as e:
             print(e)
@@ -112,6 +101,7 @@ def PopProfessorsHelper(endpoint):
 
     pass
 
+# TODO: PopProfessors() is finished!
 def PopProfessors():
     """
     Function to populate the professors table with all professors at UW-Madison. Iterates over the two RMP school IDs and calls the helper function to populate the table.
@@ -128,12 +118,12 @@ def PopProfessors():
         PopProfessorsHelper(endpoint)
     pass
 
+ # TODO: PopRedditComments() is unfinished. We will need to continually refine how we choose comments to populate the DB with!
 def PopRedditComments():
     """
     Function to populate the rc (reddit comments) table with all comments that contain a specific keyword and are posted to r/UWMadison.
     Entries contain a commend ID, the comment's text, a link to the comment, the comment's upvotes, and the cUID of the course the comment is about.
     """
-
     cursor = conn.cursor() # Create a cursor object to execute SQL queries
 
     # Query the courses table for the cCode and cUID of all courses
@@ -158,7 +148,7 @@ def PopRedditComments():
         # search = cNum
 
         # NEW METHOD: Searching for posts thathave either the full course code, or the courses acronym + course number (e.g. CS506 or CS 506)
-        # TODO: Will need to continually refine how we choose comments to populate the DB with
+       
         # Search for submissions that contain the course's code
         for submission in uwmadison_subreddit.search(search, limit=50):
             if search.lower() in submission.title.lower() or acronym + cNum in submission.title or acronym + ' ' + cNum in submission.title: # If the course's code is in the submission's title
@@ -195,66 +185,68 @@ def PopRedditComments():
 
     pass
 
-def PopTeaches(cName):
+# TODO: PopTeaches() is unfinished. All we have to do it remove "WHERE cName Like 'Introduction to Algorithms'" from first query to populate teaches with all courses!
+def PopTeaches():
     """
     Function to populate the teaches table with cUIDs and pUIDs for each course. Defining what courses each professor teaches.
     Entries contain a cUID and a pUID.
     """
-    search = cName
-    grade_distributions = MadGrades(search)
-    course_professors = []
-    all_term_data = []
 
-    # Iterate over all terms in the grade distribution data
-    for i in range(len(grade_distributions["courseOfferings"])):
-        single_term_data = grade_distributions["courseOfferings"][i]["sections"]
-        all_term_data.append(single_term_data)
+    cursor = conn.cursor() # Create a cursor object to execute SQL queries
 
-    # Iterate over all terms in the grade distribution data, and create a list of all professors that teach the course
-    num_terms = len(all_term_data)
-    for j in range(num_terms):
-        for k in range(len(all_term_data[j])):
-            course_professors.append(all_term_data[j][k]["instructors"])
-
+    cursor.execute("SELECT cUID, cCode FROM courses WHERE cName Like 'Introduction to Algorithms'") # Get the cUID, and cCode of all courses
+    courses = cursor.fetchall() 
     
-    # TODO:
-    # 1. Get the cUID of the course with course name = cName
-    # 2. Get the pUID of the professor with professor name = course_professors[i]['name']
-    # 3. Insert the tuple (cUID, pUID) into the teaches table
-    # 4. Repeat for all courses at the UW-Madison
+    for course in courses:
+        # print(course)
+        cUID = course[0]
+        cCode = course[1]
+        search = cCode
+        grade_distributions = mg.MadGrades(search)
+        course_professors = []
+        all_term_data = []
 
+        # Iterate over all terms in the grade distribution data
+        for i in range(len(grade_distributions["courseOfferings"])):
+            single_term_data = grade_distributions["courseOfferings"][i]["sections"]
+            all_term_data.append(single_term_data)
+
+        # Iterate over all terms in the grade distribution data, and create a list of all professors that teach the course
+        num_terms = len(all_term_data)
+        for j in range(num_terms):
+            for k in range(len(all_term_data[j])):
+                course_professors.append(all_term_data[j][k]["instructors"])
+
+        # print(course_professors)
+
+        for professor in course_professors:
+            prof_name = professor[0]['name']
+            cursor.execute("SELECT pUID from professors where pName Like %s", (prof_name,))
+            pUID = cursor.fetchone()
+            if pUID is not None:
+                try:
+                    cursor.execute("INSERT INTO teaches (cUID, pUID) VALUES (%s, %s)", (cUID, pUID[0],))
+                    conn.commit()
+                except Exception as e:
+                    print(e)
+                    print("Error inserting into teaches table")
+            else:
+                print("Professor not found")
+
+    cursor.close()
     pass
-
-def MadGrades(courseName):
-    """
-    Pulls grade distributions from every course at UW-Madison and populates the grade-distributions table
-    """
-    search = courseName
-    response = requests.get(madGrades_query_url + search, headers=auth_header) # API call to access list of courses matching search query
-    course_listings = response.json()
-    try:
-        course_url = course_listings['results'][0]['url']                          # Get API Endpoint URL of first course in list
-    except IndexError:
-        return
-    response = requests.get(course_url, headers=auth_header)                   # API call to get course data associated with select course
-    full_course_data = response.json()                                  
-    grades_url = full_course_data['gradesUrl']                                 # Get API Endpoint URL of grade distribution data                                            
-    response = requests.get(grades_url, headers=auth_header)                   # API call to access grade distribution data
-    courses = response.json()                                                  # Course offerings dictionary, with a list of dictionaries for each 
-    print(courses)
-    return(courses)
 
 if __name__ == '__main__':
     # PopCourses()
     # PopProfessors()
-    PopRedditComments()
-    # PopTeaches("Introduction to Algorithms")
+    # PopRedditComments()
+    PopTeaches()
     # with open('sample.json') as data_file:    
     #     data = json.load(data_file)
     #     for v in data.values():
     #         print(v['code'])
-    #         MadGrades(v['code'])
-    # print(MadGrades("ACCT I S 301"))
+    #         mg.MadGrades(v['code'])
+    # print(mg.MadGrades("ACCT I S 301"))
 
 
     

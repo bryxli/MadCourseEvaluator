b5d92fcac5ec09f592591213361a274252ddefdd
BLI378 <bli378@wisc.edu>
2022-11-01 14:02:15 -0500

    submit button

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,19 +1,26 @@
 import React, { useState } from "react";
 import { Typeahead } from "react-bootstrap-typeahead";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 const Search = () => {
   const [selected, setSelected] = useState([]);
-
   const options = ["CS 300", "CS 506", "CS 577"];
 
   return (
-    <Typeahead
-      id="basic-example"
-      onChange={setSelected}
-      options={options}
-      placeholder="Course or Instructor name"
-      selected={selected}
-    />
+    <Form method="post">
+      <Form.Label>Search</Form.Label>
+      <Typeahead
+        id="search"
+        onChange={setSelected}
+        options={options}
+        placeholder="Course or Instructor name"
+        selected={selected}
+      />
+      <Button type="submit" hidden>
+        Submit
+      </Button>
+    </Form>
   );
 };
 

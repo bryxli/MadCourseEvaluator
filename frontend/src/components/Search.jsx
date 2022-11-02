01cc0a09848381aa8a40dec727670746ae016ef7
BLI378 <bli378@wisc.edu>
2022-11-02 12:51:00 -0500

    send data to course component WORKING

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -2,15 +2,21 @@ import React, { useEffect, useState } from "react";
 import { Typeahead } from "react-bootstrap-typeahead";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
 
 const Search = () => {
   // Initialize class list
   const [classList, setClassList] = useState([]);
   useEffect(() => {
-    fetch("/").then((response) =>
-      response.json().then((data) => {
-        setClassList(data);
-      })
+    fetch("/", {
+      method: "GET",
+    }).then(
+      (
+        response // needs to be tested (make sure GET request receives the list)
+      ) =>
+        response.json().then((data) => {
+          setClassList(data);
+        })
     );
   }, []);
 
@@ -18,24 +24,30 @@ const Search = () => {
   classList;
   // const options = classList;
   const options = [
-    // Temp data
-    { id: "CS 506", name: "Software Engineering" },
-    { id: "CS 577", name: "Introduction to Algorithms" },
+    // Temp course data
+    { id: "CS506", name: "Software Engineering" },
+    { id: "CS577", name: "Introduction to Algorithms" },
   ];
 
+  // Submit button navigation
+  let navigate = useNavigate();
+  const submit = function submit() {
+    navigate(`/course?id=${encodeURIComponent(selected[0].id)}`);
+  };
+
   return (
-    <Form action="/course" method="GET">
-      <Typeahead
-        id="search"
-        onChange={setSelected}
-        labelKey={(option) => `${option.id} - ${option.name}`}
-        options={options}
-        placeholder="Course or Instructor name"
-        selected={selected}
-      />
-      <Button type="submit" hidden>
-        Submit
-      </Button>
+    <Form onSubmit={submit}>
+      <Form.Group>
+        <Typeahead
+          id="search"
+          onChange={setSelected}
+          labelKey={(option) => `${option.id} - ${option.name}`}
+          options={options}
+          placeholder="Course (Ex: CS 300)"
+          selected={selected}
+        />
+      </Form.Group>
+      <Button type="submit" hidden></Button>
     </Form>
   );
 };

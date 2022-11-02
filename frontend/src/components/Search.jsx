381cd4ff8d464906cd80631f417c7ad4770e4c71
BLI378 <bli378@wisc.edu>
2022-11-01 20:39:44 -0500

    initial GET

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,18 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Typeahead } from "react-bootstrap-typeahead";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 const Search = () => {
+  // Initialize class list
+  const [classList, setClassList] = useState([]);
+  useEffect(() => {
+    fetch("/").then((response) =>
+      response.json().then((data) => {
+        setClassList(data);
+      })
+    );
+  }, []);
+
   const [selected, setSelected] = useState([]);
-  const options = ["CS 300", "CS 506", "CS 577"];
+  classList;
+  // const options = classList;
+  const options = [
+    // Temp data
+    { id: "CS 506", name: "Software Engineering" },
+    { id: "CS 577", name: "Introduction to Algorithms" },
+  ];
 
   return (
-    <Form method="post">
-      <Form.Label>Search</Form.Label>
+    <Form action="/course" method="GET">
       <Typeahead
         id="search"
         onChange={setSelected}
+        labelKey={(option) => `${option.id} - ${option.name}`}
         options={options}
         placeholder="Course or Instructor name"
         selected={selected}

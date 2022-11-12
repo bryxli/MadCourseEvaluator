b92104c0ed07175067536568893e334ddc8261b7
Peter Bryant <72423203+peter-w-bryant@users.noreply.github.com>
2022-11-12 13:30:55 -0600

    Unit tests and app.py SQL-Alchemy changes

--- backend/tests/test_unit.py
+++ backend/tests/test_unit.py
@@ -11,24 +11,64 @@ sys.path.insert(0, parentdir)
 from app import db
 from models.models import Courses, Professors, RC, Teaches
 
+"""
 # Unit Tests: DB Models, Utility Functions called by View Functions
-def test_new_course():
-    # Create a new course
-    new_course = Courses(1, 'Intro to Computer Science', 'Computer Science', 'CS 101', '3', 'Intro to Computer Science', 'None')
+"""
 
-    # Correct Input
-    assert new_course.cUID == 1
-    assert new_course.cName == 'Intro to Computer Science'
-    assert new_course.cSubject == 'Computer Science'
-    assert new_course.cCode == 'CS 101'
-    assert new_course.cCredits == '3'
-    assert new_course.cDescription == 'Intro to Computer Science'
-    assert new_course.cReq == 'None'
+class TestModels(unittest.TestCase):
+    """
+    Tests for DB Models
+    """
 
-    print('test_new_course() passed')
+    def test_models_new_course(self):
+        """Test new course creation"""
+        new_course = Courses(1, 'Intro to Computer Science', 'Computer Science', 'CS 101', '3', 'Intro to Computer Science', 'None')
+        self.assertEqual(new_course.cUID, 1)
+        self.assertEqual(new_course.cName, 'Intro to Computer Science')
+        self.assertEqual(new_course.cSubject, 'Computer Science')
+        self.assertEqual(new_course.cCode, 'CS 101')
+        self.assertEqual(new_course.cCredits, '3')
+        self.assertEqual(new_course.cDescription, 'Intro to Computer Science')
+        self.assertEqual(new_course.cReq, 'None')
+        return True
+
+    def test_models_new_prof(self):
+        """Test new prof creation"""
+        new_prof= Professors(1, 'Good Prof', '{"name": "Good Prof", "dept": "African Studies", "RMPID": 2623431, "RMPRating": 2.0, "RMPTotalRatings": 1, "RMPRatingClass": "poor"}')
+        self.assertEqual(new_prof.pUID, 1)
+        self.assertEqual(new_prof.pName,'Good Prof')
+        self.assertEqual(new_prof.pData, '{"name": "Good Prof", "dept": "African Studies", "RMPID": 2623431, "RMPRating": 2.0, "RMPTotalRatings": 1, "RMPRatingClass": "poor"}')
+        return True
     
-    # Functional Tests: View Functions respond to nominal and non-nominal inputs (e.g. 404, 500, 200, etc.)
-    # Invalid Inputs
+    def test_models_new_RC(self):
+        """Test new reddit comment creation"""
+        comID = 1
+        comBody = "This is a comment!"
+        comLink = "https://www.reddit.com/r/UWMadison/comments/wabbh9/taking_chem_109_math_221_and_econ_101_first/ihztopb/"
+        comVotes = 13
+        cUID = 10101
+        new_reddit_comment = RC(comID, comBody, comLink, comVotes, cUID)
+        self.assertEqual(new_reddit_comment.comID, 1)
+        self.assertEqual(new_reddit_comment.comBody, "This is a comment!")
+        self.assertEqual(new_reddit_comment.comLink, "https://www.reddit.com/r/UWMadison/comments/wabbh9/taking_chem_109_math_221_and_econ_101_first/ihztopb/")
+        self.assertEqual(new_reddit_comment.comVotes, 13)
+        self.assertEqual(new_reddit_comment.cUID, 10101)
+        return True
 
+    def test_models_new_teaches(self):
+        """Test new teaches entry creation"""
+        new_teaches_entry = Teaches(1, 2, 3)
+        self.assertEqual(new_teaches_entry.tUID, 1)
+        self.assertEqual(new_teaches_entry.pUID, 2)
+        self.assertEqual(new_teaches_entry.cUID, 3)
+        return True
+    
+
+class TestUtil(unittest.TestCase):
+
+    def test_madgrades_api(self):
+        """TODO Test madgrades api"""
+        self.assertEqual(1, 1)
+    
 if __name__ == '__main__':
-    test_new_course()
+    unittest.main() # Run all unit tests

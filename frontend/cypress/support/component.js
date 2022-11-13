7a27bb58b3f3431e854bafcf7597c6b677e39cf4
BLI378 <bli378@wisc.edu>
2022-11-13 12:44:13 -0600

    Cypress.Commands to global

--- frontend/cypress/support/component.js
+++ frontend/cypress/support/component.js
@@ -20,8 +20,16 @@ import "./commands";
 // require('./commands')
 
 import { mount } from "cypress/react18";
+import { MemoryRouter } from "react-router-dom";
 
 Cypress.Commands.add("mount", mount);
+
+// Give component access to React Router
+Cypress.Commands.add("mount", (component, options = {}) => {
+  const { routerProps = { initialEntries: ["/"] }, ...mountOptions } = options;
+  const wrapped = <MemoryRouter {...routerProps}>{component}</MemoryRouter>;
+  return mount(wrapped, mountOptions);
+});
 
 // Example use:
 // cy.mount(<MyComponent />)

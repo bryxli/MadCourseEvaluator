b4e32905a7a324ac4e68499c4023a8e1699a5d30
BLI378 <bli378@wisc.edu>
2022-11-13 12:46:27 -0600

    delete filler

--- frontend/cypress/component/GPAGraph.cy.js
+++ frontend/cypress/component/GPAGraph.cy.js
@@ -1,13 +1,4 @@
 import GPAGraph from "../../src/components/GPAGraph";
-import { mount } from "cypress/react";
-import { MemoryRouter } from "react-router-dom";
-
-// Give component access to React Router
-Cypress.Commands.add("mount", (component, options = {}) => {
-  const { routerProps = { initialEntries: ["/"] }, ...mountOptions } = options;
-  const wrapped = <MemoryRouter {...routerProps}>{component}</MemoryRouter>;
-  return mount(wrapped, mountOptions);
-});
 
 describe("GPAGraph", () => {
   it("Rendered", () => {

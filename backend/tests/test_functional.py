7a388f16778cba1afd193d4f0b7021bbff4527a4
Peter Bryant <72423203+peter-w-bryant@users.noreply.github.com>
2022-11-14 18:44:05 -0600

    Added function unit test for all-profs

--- backend/tests/test_functional.py
+++ backend/tests/test_functional.py
@@ -22,7 +22,6 @@ class TestCourseRoutes(unittest.TestCase):
     Tests for DB Models
     """
     def test_all_courses(self): 
-
         request = app.test_client().get('/all-courses')   # Make a request to the /all-courses endpoint
         self.assertEqual(request.status_code, 200)        # Assert that the request was successful (200)
         self.assertEqual(request is not None, True)       # Assert that the request is not None
@@ -39,6 +38,26 @@ class TestCourseRoutes(unittest.TestCase):
             if i == 5:
                 break
 
+    def test_all_profs(self): 
+        request = app.test_client().get('/all-profs')     # Make a request to the /all-courses endpoint
+        self.assertEqual(request.status_code, 200)        # Assert that the request was successful (200)
+        self.assertEqual(request is not None, True)       # Assert that the request is not None
+        self.assertEqual(request.json is not None, True)  # Assert that the request.json is not None
+
+        # Assert that the first 5 courses contain all fields
+        i = 0
+        for key in request.json.keys():
+            self.assertEqual(request.json[key] is not None, True)
+            self.assertEqual(request.json[key]['name'] is not None, True)
+            self.assertEqual(request.json[key]['dept'] is not None, True)
+            # self.assertEqual(key, str(request.json[key]['RMPID'])) # We may want to make it so we are returning the pUID for each professor too
+            self.assertEqual("RMPID" in request.json[key].keys(), True)
+            self.assertEqual("RMPRating" in request.json[key].keys(), True)
+            self.assertEqual("RMPRatingClass" in request.json[key].keys(), True)
+            self.assertEqual("RMPTotalRatings" in request.json[key].keys(), True)
+            i += 1
+            if i == 5:
+                break
 
     
     

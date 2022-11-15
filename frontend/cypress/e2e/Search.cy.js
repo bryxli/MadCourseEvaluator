e947efc3693cec1d1df7c6501433b93411ce3c62
BLI378 <bli378@wisc.edu>
2022-11-15 17:14:29 -0600

    change wait

--- frontend/cypress/e2e/Search.cy.js
+++ frontend/cypress/e2e/Search.cy.js
@@ -1,11 +1,12 @@
+/* eslint-disable cypress/no-unnecessary-waiting */
+
 describe("Search", () => {
   it("Search bar successfully retrieves all courses", () => {
     cy.visit("localhost:3000/");
     cy.get("input").then((search_bar) => {
       search_bar.click();
       // Wait for endpoint to load data
-      cy.intercept("/courselist").as("courselist");
-      cy.wait("@courselist");
+      cy.wait(1000);
       cy.get("div[id='search']").then((result) => {
         const search = result[1].children;
         // Replace 3 with number of courses to retrieve from endpoint

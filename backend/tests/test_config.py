414eea8c3a4dacd8918e6ff8d8f44b96c07e2a38
Peter Bryant <72423203+peter-w-bryant@users.noreply.github.com>
2022-11-29 15:13:19 -0600

    Test config now updates test UID, test_functional updates UIDs when it is run

--- backend/tests/test_config.py
+++ backend/tests/test_config.py
@@ -1,5 +1,36 @@
-# CS 577 cUID
-cs577_cUID = '58874'
+import mysql.connector
+import os
+import inspect
+import sys
 
-# Marc Renault pUID
-marc_renault_pUID = '136944'
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir) 
+
+import config
+
+"""
+Run test_config.py to update the UID for all test data in the database.
+"""
+
+# Establish connection to MySQL DB
+conn = mysql.connector.connect(
+    user = config.user,
+    password = config.password, 
+    host = config.host,
+    database = config.database
+)
+    
+cur = conn.cursor()
+
+# Update COMP SCI 577 cUID for testing
+cur.execute("SELECT cUID FROM courses WHERE cCode = 'Comp Sci 577'")
+cs577_cUID = str(cur.fetchone()[0])
+
+# Update Marc Renault pUID
+cur.execute("SELECT pUID FROM professors WHERE pName = 'Marc Renault'")
+marc_renault_pUID = str(cur.fetchone()[0])
+
+cur.close()
+conn.close()
+    

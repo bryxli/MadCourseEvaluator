3bcae75b5e9da646bd3e684c4b97837fdb262083
Peter Bryant <72423203+peter-w-bryant@users.noreply.github.com>
2022-12-13 10:19:15 -0600

    Code submission formatting

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 __author__ = "Peter Bryant, Jarvis Jia"
-__credits__ = ["Peter Bryant", "Jarvis Jia", "Bryan Li", "Swathi Annamaneni", "Aidan Shine"]
+__credits__ = ["Peter Bryant", "Jarvis Jia",
+               "Bryan Li", "Swathi Annamaneni", "Aidan Shine"]
 __version__ = "1.0.0"
 __maintainer__ = "Peter Bryant"
 __email__ = "pbryant2@wisc.edu"
@@ -27,7 +28,7 @@ Flask application for MadCourseEvaluator back end web API.
 
 # Flask Imports
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy # SQLAlchemy ORM
+from flask_sqlalchemy import SQLAlchemy  # SQLAlchemy ORM
 from flask_cors import CORS             # CORS: Cross Origin Resource Sharing
 
 # Python Imports
@@ -35,29 +36,37 @@ from sqlalchemy import create_engine    # SQLAlchemy Engine
 import json
 
 # Custom Scripts
-import madgrades as mg                  # Custom MadGrades Script for Grade Distributions
+# Custom MadGrades Script for Grade Distributions
+import madgrades as mg
 import config                           # Application Configuration
 
-db_uri =  'mysql://' + config.user + ':' + config.password + '@' + config.host + '/' + config.database
+db_uri = 'mysql://' + config.user + ':' + config.password + '@' + config.host + '/' + config.database
 db = SQLAlchemy()
 
-app = Flask(__name__) 
+app = Flask(__name__)
 CORS(app)                                       # Enable CORS for all routes
 app.config['CORS_HEADERS'] = 'Content-Type'     # Set CORS header
 app.secret_key = config.secret                  # Set Flask Secret Key
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri  # Set SQLAlchemy URI
 engine = create_engine(db_uri)                  # Create SQLAlchemy Engine
 
-@app.route('/all-courses', methods=['GET','POST'])
+
+@app.route('/all-courses', methods=['GET', 'POST'])
 def AllCourses():
     """
-    All Courses endpoint: Returns JSON of all courses at the university along with all fields associated with each course.
+    Returns JSON of all courses at the university along with all fields associated with each course.
+
+    Args:
+        None
+
+    Returns:
+        all_course_json_data (dict): Dictionary of all courses at the university along with all fields associated with each course.
     """
     conn = engine.raw_connection()
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM courses") # Store all data on all courses
+    cursor.execute("SELECT * FROM courses")  # Store all data on all courses
 
-    if cursor.rowcount == 0: # If no rows are returned, return an empty dictionary with key 'error'
+    if cursor.rowcount == 0:  # If no rows are returned, return an empty dictionary with key 'error'
         return {'error': 'No courses found for route /all-courses.'}
 
     all_courses = cursor.fetchall()
@@ -67,24 +76,31 @@ def AllCourses():
     for course in all_courses:
         course_json_data = {'cUID': None, 'cName': None, 'cCode': None}
         course_json_data['cUID'] = course[0]
-        course_json_data['cName'] = course[1] 
-        course_json_data['cCode'] = course[3] 
+        course_json_data['cName'] = course[1]
+        course_json_data['cCode'] = course[3]
         all_course_json_data[course_json_data['cUID']] = course_json_data
 
-    cursor.close() 
-    conn.close()   
+    cursor.close()
+    conn.close()
     return all_course_json_data
 
-@app.route('/all-profs', methods=['GET','POST'])
+
+@app.route('/all-profs', methods=['GET', 'POST'])
 def AllProfs():
     """
-    All Professors endpoint: returns a dictionary of all professors at the university along with all fields associated with each professor.
+    Returns a dictionary of all professors at the university along with all fields associated with each professor.
+
+    Args:
+        None
+
+    Returns:
+        all_profs_json_data (dict): Dictionary of all professors at the university along with all fields associated with each professor.
     """
     conn = engine.raw_connection()
     cursor = conn.cursor()
-    cursor.execute("SELECT pUID, pData FROM professors") # Execute SQL query
+    cursor.execute("SELECT pUID, pData FROM professors")  # Execute SQL query
 
-    if cursor.rowcount == 0: # If no rows are returned, return an empty dictionary with key 'error'
+    if cursor.rowcount == 0:  # If no rows are returned, return an empty dictionary with key 'error'
         return {'error': 'No professors found for route /all-profs.'}
 
     all_profs = cursor.fetchall()  # Fetch all the rows from professors table
@@ -100,24 +116,33 @@ def AllProfs():
     conn.close()
     return all_profs_json_data
 
-@app.route('/course-info/<cUID>', methods=['GET','POST'])
+
+@app.route('/course-info/<cUID>', methods=['GET', 'POST'])
 def courseInfo(cUID):
     """
-    Returns all course information from courses table.
+    Returns all of a single course's information from courses table corresponding to the given cUID.
+
+    Args:
+        cUID (str): Course Unique ID
+
+    Returns:
+        course_json_data (dict): Dictionary of all fields associated with the course corresponding to the given cUID.
     """
     conn = engine.raw_connection()
     cursor = conn.cursor()
-    
-    # Get the corresponding course data from courses table:
-    cursor.execute("SELECT * FROM courses WHERE cUID = %s", (cUID,))   # Execute SQL query
 
-    if cursor.rowcount == 0: # If no rows are returned, return an empty dictionary with key 'error'
+    # Get the corresponding course data from courses table:
+    cursor.execute("SELECT * FROM courses WHERE cUID = %s",
+                   (cUID,))   # Execute SQL query
+
+    if cursor.rowcount == 0:  # If no rows are returned, return an empty dictionary with key 'error'
         return {'error': 'No professors found for route /course-info/' + cUID + '.'}
 
-    course_data = cursor.fetchall()[0]                                 # Fetch all the course data for the given cUID
-    course_json_data = {'cUID': None, 'cName': None, 'cSubject': None, # Create a dictionary to store course data
-                        'cCode': None, 'cCredits': None, 'cDescription': None, 'cReq': None} 
-    
+    # Fetch all the course data for the given cUID
+    course_data = cursor.fetchall()[0]
+    course_json_data = {'cUID': None, 'cName': None, 'cSubject': None,  # Create a dictionary to store course data
+                        'cCode': None, 'cCredits': None, 'cDescription': None, 'cReq': None}
+
     # Populate the course dictionary
     course_json_data['cUID'] = course_data[0]
     course_json_data['cName'] = course_data[1]
@@ -128,26 +153,28 @@ def courseInfo(cUID):
     course_json_data['cReq'] = course_data[6]
 
     cursor.close()
-    conn.close() 
+    conn.close()
     return course_json_data
 
-@app.route('/course-profs/<cUID>', methods=['GET','POST'])
+
+@app.route('/course-profs/<cUID>', methods=['GET', 'POST'])
 def courseProfs(cUID):
     """
     Returns all professors who have taught the course recently.
     """
     conn = engine.raw_connection()
     cursor = conn.cursor()
-    
-    q = "SELECT pUID, pData FROM professors WHERE pUID IN (SELECT pUID FROM teaches WHERE cUID = %s)"
-    cursor.execute(q, (cUID,)) # Get all the professors who have taught the course recently
 
-    if cursor.rowcount == 0: # If no rows are returned, return an empty dictionary with key 'error'
+    q = "SELECT pUID, pData FROM professors WHERE pUID IN (SELECT pUID FROM teaches WHERE cUID = %s)"
+    # Get all the professors who have taught the course recently
+    cursor.execute(q, (cUID,))
+
+    if cursor.rowcount == 0:  # If no rows are returned, return an empty dictionary with key 'error'
         return {'error': 'No professors found for route /course-profs/' + cUID + '.'}
 
-    all_profs = cursor.fetchall() # Fetch all the rows from professors table
+    all_profs = cursor.fetchall()  # Fetch all the rows from professors table
 
-    all_prof_json_data = {} # Create a dictionary mapping pUID to pData
+    all_prof_json_data = {}  # Create a dictionary mapping pUID to pData
 
     # Populate the all_prof_json_data dictionary
     for prof in all_profs:
@@ -159,22 +186,24 @@ def courseProfs(cUID):
     conn.close()
     return all_prof_json_data
 
-@app.route('/reddit-comments/<cUID>', methods=['GET','POST'])
+
+@app.route('/reddit-comments/<cUID>', methods=['GET', 'POST'])
 def redditComments(cUID):
     """
     Returns all Reddit comments associated with the course.
     """
     conn = engine.raw_connection()
     cursor = conn.cursor()
-    
-    cursor.execute("SELECT * FROM rc WHERE cUID = %s", (cUID,)) # Get all Reddit comments associated with the course
 
-    if cursor.rowcount == 0: # If no rows are returned, return an empty dictionary with key 'error'
+    # Get all Reddit comments associated with the course
+    cursor.execute("SELECT * FROM rc WHERE cUID = %s", (cUID,))
+
+    if cursor.rowcount == 0:  # If no rows are returned, return an empty dictionary with key 'error'
         return {'error': 'No professors found for route /reddit-comments/' + cUID + '.'}
 
     all_rc = cursor.fetchall()  # Fetch all the rows from rc table
 
-    all_rc_json_data = {} # Create a dictionary mapping rcUID to rcData
+    all_rc_json_data = {}  # Create a dictionary mapping rcUID to rcData
 
     # Populate the all_rc_json_data dictionary with all Reddit comment data
     for rc in all_rc:
@@ -182,13 +211,15 @@ def redditComments(cUID):
         comBody = rc[1]
         comLink = rc[2]
         comVotes = rc[3]
-        all_rc_json_data[comID] = {'comBody': comBody, 'comLink': comLink, 'comVotes': comVotes}
+        all_rc_json_data[comID] = {'comBody': comBody,
+                                   'comLink': comLink, 'comVotes': comVotes}
 
     cursor.close()
     conn.close()
     return all_rc_json_data
 
-@app.route('/grade-distribution/<cUID>', methods=['GET','POST'])
+
+@app.route('/grade-distribution/<cUID>', methods=['GET', 'POST'])
 def gradeDistribution(cUID):
     """
     Returns grade distributions for the provided course.
@@ -196,35 +227,41 @@ def gradeDistribution(cUID):
     conn = engine.raw_connection()
     cursor = conn.cursor()
 
-    cursor.execute("SELECT cCode FROM courses WHERE cUID = %s", (cUID,)) # Get the course code for the course
-    cCode= cursor.fetchall()[0][0] # Fetch the course code and strip the whitespace
+    cursor.execute("SELECT cCode FROM courses WHERE cUID = %s",
+                   (cUID,))  # Get the course code for the course
+    # Fetch the course code and strip the whitespace
+    cCode = cursor.fetchall()[0][0]
 
-    if cursor.rowcount == 0: # If no rows are returned, return an empty dictionary with key 'error'
+    if cursor.rowcount == 0:  # If no rows are returned, return an empty dictionary with key 'error'
         return {'error': 'No courses found for route /grade-distribution/' + cUID + '.'}
-  
-    grade_distribution = mg.MadGrades(cCode) # Get grade distribution for the course using the course code
+
+    # Get grade distribution for the course using the course code
+    grade_distribution = mg.MadGrades(cCode)
 
     # Get the cumulative grade distribution for each professor
     grade_distribution['professor_cumulative_grade_distribution'] = {}
 
     # Get the name of all professors who have taught the course in the database
-    cursor.execute("SELECT p.pUID, p.pName from professors p, courses c, teaches t WHERE c.cCode = %s and c.cUID = t.cUID and p.pUID = t.pUID", (cCode,))
+    cursor.execute(
+        "SELECT p.pUID, p.pName from professors p, courses c, teaches t WHERE c.cCode = %s and c.cUID = t.cUID and p.pUID = t.pUID", (cCode,))
 
-    if cursor.rowcount == 0: # If no rows are returned, return an empty dictionary with key 'error'
+    if cursor.rowcount == 0:  # If no rows are returned, return an empty dictionary with key 'error'
         return {'error': 'No professors found for route /grade-distribution/' + cUID + '.'}
 
-    course_profs = cursor.fetchall() # Fetch all the professor names for professors who have taught the course
+    # Fetch all the professor names for professors who have taught the course
+    course_profs = cursor.fetchall()
 
     # For each professor, populate the grade_distribution dictionary with their cumulative grade distribution (sum over all sections)
     for prof_info in course_profs:
         prof_pUID = prof_info[0]
         prof_name = prof_info[1]
-        grade_distribution['professor_cumulative_grade_distribution'][prof_pUID] = {}
+        grade_distribution['professor_cumulative_grade_distribution'][prof_pUID] = {
+        }
         grade_distribution['professor_cumulative_grade_distribution'][prof_pUID]['name'] = prof_name
         grade_distribution['professor_cumulative_grade_distribution'][prof_pUID]['aCount'] = 0
         grade_distribution['professor_cumulative_grade_distribution'][prof_pUID]['abCount'] = 0
         grade_distribution['professor_cumulative_grade_distribution'][prof_pUID]['bCount'] = 0
-        grade_distribution['professor_cumulative_grade_distribution'][prof_pUID]['bcCount'] = 0 
+        grade_distribution['professor_cumulative_grade_distribution'][prof_pUID]['bcCount'] = 0
         grade_distribution['professor_cumulative_grade_distribution'][prof_pUID]['cCount'] = 0
         grade_distribution['professor_cumulative_grade_distribution'][prof_pUID]['crCount'] = 0
         grade_distribution['professor_cumulative_grade_distribution'][prof_pUID]['dCount'] = 0
@@ -244,18 +281,21 @@ def gradeDistribution(cUID):
             for i in range(len(grade_distribution["courseOfferings"])):
                 for j in range(len(range(len(grade_distribution["courseOfferings"][i]['sections'])))):
                     for k in range(len(grade_distribution["courseOfferings"][i]['sections'][j]['instructors'])):
-                        API_prof_name = grade_distribution["courseOfferings"][i]['sections'][j]['instructors'][k]['name']
+                        API_prof_name = grade_distribution["courseOfferings"][
+                            i]['sections'][j]['instructors'][k]['name']
                         # If the professor name in the API contains "X / ", remove the "X / " from the name
                         if "X / " in API_prof_name:
                             API_prof_name = API_prof_name.split("X / ")[1]
                         # If the professor name is in the returned API data, add the grade counts to the cumulative grade distribution for the professor
-                        if API_prof_name== prof_name.upper():
-                            grade_distribution['professor_cumulative_grade_distribution'][prof_pUID][key] += grade_distribution["courseOfferings"][i]['sections'][j][key]
+                        if API_prof_name == prof_name.upper():
+                            grade_distribution['professor_cumulative_grade_distribution'][prof_pUID][
+                                key] += grade_distribution["courseOfferings"][i]['sections'][j][key]
     cursor.close()
     conn.close()
     return grade_distribution
 
-@app.route('/prof-info/<pUID>', methods=['GET','POST'])
+
+@app.route('/prof-info/<pUID>', methods=['GET', 'POST'])
 def professorInfo(pUID):
     """
     Specific Professor Info endpoint: returns all RateMyProfessor data for a professor
@@ -264,30 +304,34 @@ def professorInfo(pUID):
     conn = engine.raw_connection()
     cursor = conn.cursor()
 
-    cursor.execute("SELECT pData FROM professors WHERE pUID = %s", (pUID,)) # Get professor data from professors table
+    # Get professor data from professors table
+    cursor.execute("SELECT pData FROM professors WHERE pUID = %s", (pUID,))
 
-    if cursor.rowcount == 0: # If no rows are returned, return an empty dictionary with key 'error'
+    if cursor.rowcount == 0:  # If no rows are returned, return an empty dictionary with key 'error'
         return {'error': 'No professors found for route /prof-info/' + pUID + '.'}
 
     prof_data = cursor.fetchall()[0][0]    # Fetch all the professor data
-    professor_data = json.loads(prof_data) # Store professor data in the full professor data json that will be returned
+    # Store professor data in the full professor data json that will be returned
+    professor_data = json.loads(prof_data)
 
     cursor.close()
     conn.close()
     return professor_data
 
-@app.route('/prof-courses/<pUID>', methods=['GET','POST'])
+
+@app.route('/prof-courses/<pUID>', methods=['GET', 'POST'])
 def professorCourses(pUID):
     """
     Returns all courses taught by a professor associated with the given pUID.
     """
-    conn= engine.raw_connection()
+    conn = engine.raw_connection()
     cursor = conn.cursor()
 
-    cursor.execute("SELECT cUID FROM teaches WHERE pUID = %s", (pUID,)) # Get the course ID data from teaches table
+    # Get the course ID data from teaches table
+    cursor.execute("SELECT cUID FROM teaches WHERE pUID = %s", (pUID,))
     list_courseID = cursor.fetchall()
 
-    if cursor.rowcount == 0: # If no rows are returned, return an empty dictionary with key 'error'
+    if cursor.rowcount == 0:  # If no rows are returned, return an empty dictionary with key 'error'
         return {'error': 'No courses found for route /prof-courses/' + pUID + '.'}
 
     full_course_data_json = {}
@@ -295,11 +339,13 @@ def professorCourses(pUID):
     # For each course, get the course data from the courses table
     for courseID in list_courseID:
         courseID = str(courseID[0])
-        cursor.execute("SELECT cName, cSubject, cCode, cCredits, cDescription, cReq FROM courses WHERE cUID = %s", (courseID,)) 
+        cursor.execute(
+            "SELECT cName, cSubject, cCode, cCredits, cDescription, cReq FROM courses WHERE cUID = %s", (courseID,))
         course_data = cursor.fetchall()
 
         # Store course data in dictionary
-        course_json_data = {'cName': None, 'cSubject': None, 'cCode': None, 'cCredits': None, 'cDescription': None, 'cReq': None} 
+        course_json_data = {'cName': None, 'cSubject': None, 'cCode': None,
+                            'cCredits': None, 'cDescription': None, 'cReq': None}
         course_data = course_data[0]
         course_json_data['cName'] = course_data[0]
         course_json_data['cSubject'] = course_data[1]
@@ -321,5 +367,6 @@ def professorCourses(pUID):
     conn.close()
     return full_course_data_json
 
+
 if __name__ == '__main__':
-    app.run(debug=True, port = 5000)
+    app.run(debug=True, port=5000)

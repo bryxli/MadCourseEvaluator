5a41237ffcc5b218d7a1c9393ad4fc840acfbcf1
jia44 <jia44@wisc.edu>
2022-12-12 21:33:58 -0600

    Add author info

--- backend/course_scrape/test.py
+++ backend/course_scrape/test.py
@@ -1,3 +1,8 @@
+__author__ = "Jarvis Jia"
+__credits__ = ["Peter Bryant", "Jarvis Jia", "Bryan Li", "Swathi Annamaneni", "Aidan Shine"]
+__version__ = "1.0.0"
+__maintainer__ = "Jarvis Jia"
+
 import json
 from fetch import ScrapeCourses
 def ScrapeCoursesTest():
